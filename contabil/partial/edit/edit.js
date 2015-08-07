9d812207baca98a8f18ca34ebcedc800c0d0f2c4
denisra <denis.afonso@gmail.com>
2015-08-07 02:14:16 +0000

    Created the edit module

---
+++ contabil/partial/edit/edit.js
@@ -0,0 +1,8 @@
+angular.module('edit', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('edit').config(function($stateProvider) {
+
+    /* Add New States Above */
+
+});
+
---
+++ contabil/partial/edit/editController.js
@@ -0,0 +1,4 @@
+angular.module('edit').controller('EditCtrl',function($scope){
+
+
+});
