282a6cb0aa62556d50962342e984adc154fcdced
denisra <denis.afonso@gmail.com>
2015-08-07 22:28:36 +0000

    Added some initial functionality to editController

--- contabil/partial/edit/edit.js
+++ contabil/partial/edit/edit.js
@@ -2,6 +2,12 @@ angular.module('edit', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
 
 angular.module('edit').config(function($stateProvider) {
 
+    $stateProvider.state('edit', {
+			//parent: 'contabil',
+			url: '/contabil/edit/:recordId',
+			controller: 'EditCtrl',
+			templateUrl: 'contabil/partial/create/create.html'
+    });
     /* Add New States Above */
 
 });
--- contabil/partial/edit/editController.js
+++ contabil/partial/edit/editController.js
@@ -1,4 +1,37 @@
-angular.module('edit').controller('EditCtrl',function($scope){
+angular.module('edit').controller('EditCtrl',function($scope, $firebaseArray, $rootScope, $state, $stateParams){
 
+	var recordId = $stateParams.recordId;
 
+	$rootScope.showSearch = ($state.current.name === 'contabil');
+	console.log('contabil state: ', $state.current.name);
+
+	var ref = new Firebase("https://alugueis.firebaseio.com/contabil");
+
+	function convertDate (record) {
+		_.mapObject(record, function(val, key) {
+			if (key.indexOf('date') > 0) {
+				var d = new Date(record[key]);
+				record[key] = d;
+				console.log('date obj: ', record[key]);
+			}
+		$scope.record = record;
+
+		});
+
+	}
+
+	function loadData () {
+		var data = $firebaseArray(ref);
+		data.$loaded()
+		.then (function (data) {
+			console.log('param : ', recordId);
+			var record = _.find(data, function(obj) { return obj.$id === recordId;});
+			console.log('record : ', record);
+			convertDate(record);
+			//console.log('scope data: ', $scope.mycontent);
+			//console.log('total amounts: ', $scope.total_amounts);
+		});
+	}
+
+	loadData();
 });
