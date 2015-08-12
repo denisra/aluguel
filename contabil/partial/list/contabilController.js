375fb5595954d97b2f9541e9bff91dcf2d7f5173
denisra <denis.afonso@gmail.com>
2015-08-12 16:19:35 -0500

    cleaned up some of the controllers

--- contabil/partial/list/contabilController.js
+++ contabil/partial/list/contabilController.js
@@ -1,7 +1,6 @@
 angular.module('contabil').controller('ContabilCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$state', '$rootScope', '$mdDialog', 'recordService', 
 	function($scope, $firebaseObject, $firebaseArray, $state, $rootScope, $mdDialog, recordService){
 
-	//var tmp;
 	$scope.mycontent = [];
 
 	$rootScope.showSearch = recordService.getState();
@@ -17,15 +16,12 @@ angular.module('contabil').controller('ContabilCtrl', ['$scope', '$firebaseObjec
 		});
 	};
 
-
 	$scope.toggleSearch = false;
 	$scope.custom = {name: 'bold', start_date:'grey', end_date:'grey', due_date:'grey', total_due:'grey', total_received: 'grey'};
   $scope.sortable = ['name', 'start_date', 'end_date', 'due_date', 'total_due', 'total_received'];
   $scope.thumbs = '';
   $scope.count = 5;
   $scope.filters = 'search';
-
-
 
 	$scope.headers = [
 		{
@@ -49,19 +45,6 @@ angular.module('contabil').controller('ContabilCtrl', ['$scope', '$firebaseObjec
     }
   ];
 
-  //$scope.content = ref.
-
-	// $scope.content = [{name: 'Denis Afonso', start_date: '01-07-2015', end_date: '15-07-2015', total_due: '2500', total_received: '500', due_date: '15-06-2015'},
-	// 	{name: 'Mona Afonso', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'}, 
-	// 	{name: 'Mara Afonso', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'}, 
-	// 	{name: 'Domingos Fernandes', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'}, 
-	// 	{name: 'Pippa Afonso', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'},
-	// 	{name: 'Debora Afonso', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'},
-	// 	{name: 'Eoin Wilde', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'},
-	// 	{name: 'Jimmy Wilde', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'},
-	// 	{name: 'Julie Wilde', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'},
-	// 	{name: 'Mama Brusqueta', start_date: '25-11-2015', end_date: '01-12-2015', total_due: '2100', total_received: '1000', due_date: '01-10-2015'}];
-
 	$scope.deleteRecord = function (record) {
 	  $mdDialog.show(
 	    $mdDialog.confirm()
@@ -80,8 +63,5 @@ angular.module('contabil').controller('ContabilCtrl', ['$scope', '$firebaseObjec
 	  });
 	};
 
-
 	loadData();
-
-
 }]);
